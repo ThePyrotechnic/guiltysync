0faa1380d8a34212650c89d4aa864865fbf89011
Michael Manis <michaelmanis@tutanota.com>
2023-01-31 17:51:59 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="guiltysync",
-    version="0.11.0",
+    version="0.12.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
